1d0d62a1e8d0c687d19ef6df0154de3555f374d8
Farid-Karimli <karimli.farid@icloud.com>
2025-10-27 11:19:09 -0400

    Minor additions

--- finetuning/SWIN/constants.py
+++ finetuning/SWIN/constants.py
@@ -1,1 +1,4 @@
-LARGE_384_22K = "microsoft/swin-large-patch4-window7-224-in22k" 
+LARGE_384_22K = "microsoft/swin-large-patch4-window7-224-in22k"
+
+BASE_224_22K = "microsoft/swin-base-patch4-window7-224-in22k"
+BASE_192_22K = "microsoft/swinv2-base-patch4-window12-192-22k"
