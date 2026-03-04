b631cd667ab84bbbb7841596f6efe2cbf18f1f6e
Farid-Karimli <karimli.farid@icloud.com>
2026-03-04 15:22:42 -0500

    Model config constant

--- finetuning/SWIN/constants.py
+++ finetuning/SWIN/constants.py
@@ -1,6 +1,7 @@
 LARGE_224_22K = "microsoft/swin-large-patch4-window7-224-in22k"
 LARGE_384_22k = "microsoft/swin-large-patch4-window12-384-in22k"
 
+BASE_384_22K = "microsoft/swin-base-patch4-window12-384-in22k"
 BASE_224_22K = "microsoft/swin-base-patch4-window7-224-in22k"
 BASE_192_22K = "microsoft/swinv2-base-patch4-window12-192-22k"
 
