509a7194fb6d97f783f5d15b1a4399ce08948a8b
Farid-Karimli <karimli.farid@icloud.com>
2026-03-23 10:59:49 -0400

    train_advanced file

--- finetuning/SWIN/train_advanced.sh
+++ finetuning/SWIN/train_advanced.sh
@@ -12,7 +12,7 @@ conda activate herb_env
 #   - configs_advanced/swinv2_base_192_enhanced.yml
 # If CONFIG_FILE is not set (e.g., via qsub -v), use default
 if [ -z "$CONFIG_FILE" ]; then
-    CONFIG_FILE="configs/swin_large_unfrozen_15k.yml"
+    CONFIG_FILE="configs_advanced/swin_base_224_multitask.yml"
 fi
 
 echo "Using config file: $CONFIG_FILE"
@@ -20,4 +20,4 @@ echo "Using config file: $CONFIG_FILE"
 python SWIN_finetuning_advanced.py --config $CONFIG_FILE
 
 # Example qsub command for multi-GPU training:
-# qsub -l h_rt=48:00:00 -pe omp 16 -P herbdl -l gpus=2 -l gpu_c=8.0 -l gpu_memory=80G -m beas -M faridkar@bu.edu -N SWIN_Large_50K train_advanced.sh
+# qsub -l h_rt=48:00:00 -pe omp 16 -P herbdl -l gpus=2 -l gpu_c=8.0 -l gpu_memory=80G -m beas -M faridkar@bu.edu -N SWINB_MT train_advanced.sh
